c0638e6ba1eb592c0a36cd5ba35bec6e8a431bda
Echolinq <echolinq@protonmail.com>
2021-10-10 17:55:46 -0400

    initial commit

--- client/index.js
+++ client/index.js
@@ -1,6 +1,21 @@
 import "./index.scss";
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const SHA256 = require("crypto-js/sha256");
 
 const server = "http://localhost:3042";
+
+// 
+// Used to auto load addresses into inputs for debugging
+// 
+// window.addEventListener('load', () => {
+//     fetch(`${server}/addresses`).then((response) => {
+//       return response.json();
+//     }).then(({ address1, address2}) => {
+//       document.getElementById("exchange-address").value = address1;
+//       document.getElementById("recipient").value = address2;
+//     });
+// });
 
 document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
   if(value === "") {
@@ -19,16 +34,50 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  
+  //
+  // simple verification on input parameters
+  //
+  if(sender === "") {
+    alert('must enter address in wallet')
+    return
+  }
+
+  if(sender === recipient) {
+    alert('wallet address and recipient cannot be same address')
+    return
+  } 
+  
+  if(amount == '') {
+    alert('must enter an amount')
+    return
+  }
+  const tx = {
+    sender, amount, recipient, 
+  }
+  
+  //
+  // get private key from user and sign transaction
+  //
+  const privateKey = prompt('Enter Private key to sign transaction')
+  const hash = SHA256(JSON.stringify(tx)).toString()  
+  const sig = ec.sign(hash, privateKey,"hex", {canonical: true});
 
   const body = JSON.stringify({
-    sender, amount, recipient
+    tx, sig
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
 
   fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
     return response.json();
-  }).then(({ balance }) => {
+  }).then(({ isValid, balance }) => {
     document.getElementById("balance").innerHTML = balance;
+    if(isValid) {
+      alert(`Tranaction successfull sent $${tx.amount} to ${tx.recipient}`)
+    } else {
+      alert('Transaction failed')
+    }
+
   });
 });
--- server/index.js
+++ server/index.js
@@ -1,30 +1,87 @@
-const express = require('express');
+const express = require("express");
+const EC = require("elliptic").ec;
+const SHA256 = require("crypto-js/sha256");
+const cors = require("cors");
 const app = express();
-const cors = require('cors');
 const port = 3042;
+const ec = new EC("secp256k1");
+const balances = {};
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
-}
+const key1 = ec.genKeyPair();
+const key2 = ec.genKeyPair();
+const key3 = ec.genKeyPair();
 
-app.get('/balance/:address', (req, res) => {
-  const {address} = req.params;
+
+const address1 = SHA256(key1.getPublic().encode("hex")).toString().slice(-40);
+const address2 = SHA256(key2.getPublic().encode("hex")).toString().slice(-40);
+const address3 = SHA256(key3.getPublic().encode("hex")).toString().slice(-40);
+
+balances[address1] = 100;
+balances[address2] = 50;
+balances[address3] = 75;
+
+console.log(`
+AVAILABLE ACCOUNTS
+===================
+${address1} (${balances[address1]})
+${address2} (${balances[address2]})
+${address3} (${balances[address3]})
+
+PRIVATE KEYS
+=============
+${key1.getPrivate('hex')}
+${key2.getPrivate('hex')}
+${key3.getPrivate('hex')}
+`);
+
+app.get("/balance/:address", (req, res) => {
+  const { address } = req.params;
   const balance = balances[address] || 0;
   res.send({ balance });
 });
 
-app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+app.post("/send", (req, res) => {
+  const { tx, sig } = req.body;
+
+  //tx.recipient = '377b1cdcb7148d1081e7d9b57286e42c9d9d5c9b' 
+  //tx.amount = 377
+  
+  // hash transaction for verification
+  let msg = SHA256(JSON.stringify(tx)).toString();
+
+  // use signature and tranaction has to recover the public key for verification
+  let hexToDecimal = (x) => ec.keyFromPrivate(x, "hex").getPrivate().toString(10);
+  const recoveredPublicKey = ec.recoverPubKey(hexToDecimal(msg), sig, sig.recoveryParam, 'hex');
+
+  // recover the address of the sender
+  recoveredAddress = SHA256(recoveredPublicKey.encode("hex")).toString().slice(-40);
+
+  // Checks that the recovered address is the same as the sender of the transaction
+  const isValid = () =>{
+    if(recoveredAddress === tx.sender)
+      return true
+    return false
+  } 
+
+  if (isValid()) {
+    balances[tx.sender] -= tx.amount;
+    balances[tx.recipient] = (balances[tx.recipient] || 0) + +tx.amount;
+    res.send({ isValid: true, balance: balances[tx.sender] });
+  } else {
+    res.send({ isValid: false, balance: balances[tx.sender] });
+  }
+});
+
+//
+// Enpoint used for debugging to auto load address into form inputs
+//
+app.get("/addresses", (req, res) => {
+  res.send({ address1, address2});
 });
 
 app.listen(port, () => {
